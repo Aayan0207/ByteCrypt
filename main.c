043fe0af7c9db7411c8a9bb7f4030dd53810e9ff
Aayan Khan <aayan.khan02072007@gmail.com>
2025-11-25 10:33:33 +0530

    Slight change

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int seed(char password[]);
 int main(int argc, char* argv[]) {
     printf("    ____        __       ______                 __ \n   / __ )__  __/ /____  / ____/______  ______  / /_\n  / __  / / / / __/ _ \\/ /   / ___/ / / / __ \\/ __/\n / /_/ / /_/ / /_/  __/ /___/ /  / /_/ / /_/ / /_  \n/_____/\\__, /\\__/\\___/\\____/_/   \\__, / .___/\\__/  \n      /____/                    /____/_/           \n");
     if (argc != 3) {
-        printf("\x1B[31mUsage: program.exe <file_path> <password>\x1B[0m");
+        printf("\x1B[31mUsage: <file_path> <password>\x1B[0m");
         return 0;
     }
     char file[256], password[32];
